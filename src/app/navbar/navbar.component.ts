a4e1533f8664cd8898d33163bcc73a7af4379d7f
Shereen-Esther <shereenjubal@gmail.com>
2024-06-14 22:45:53 -0400

    Function for scrolling and nav bar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,10 +12,22 @@ export class NavbarComponent {
   scrollToSection(section: string): void {
     this.scrollService.scrollToAnchor(section);
   }
-  scrollTo(section: string): void {
+  scrollTo(section: any): void {
+    const scrolll = window.scrollY || document.documentElement.scrollTop;
     const element = document.getElementById(section);
+    const navlinks = document.querySelectorAll('a');
+    navlinks.forEach(link => {
+      let sec: any = document.querySelector(link.getAttribute('href') as string);
+      if (sec.offsetTop <= scrolll && (sec.offsetTop + sec.offsetHeight) > scrolll) {
+        link?.classList.add('active');
+      } else {
+        link?.classList.remove('active');
+      }
+    });
     if (element) {
       element.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
     }
   }
+
+
 }
